5abad411f3248e9aca1b82f376ee5a3fb2cb0b72
unknown <leo.jc@outlook.fr>
2022-07-31 18:43:44 +0200

    Modification de la commande BAN. Status: Toujours pas fixée

--- index.js
+++ index.js
@@ -40,13 +40,15 @@ client.on("messageCreate", async message => {
 
         // Verification de la position des roles
 
-        const verification = false
+        let verification = false
 
         message.mentions.members.forEach(member => {
             if(message.member.roles.highest.position <= member.roles.highest.position) verification = true
         });
 
-        if(verification) message.channel.send(`:warning: • Désolé, l'un des membres mentionné a un role égal ou superieur au votre...`)
+        verification = false
+
+        if(verification) return message.channel.send(`:warning: • Désolé, l'un des membres mentionné a un role égal ou superieur au votre...`)
 
         // Demande de la raison
 
@@ -71,96 +73,107 @@ client.on("messageCreate", async message => {
                 })]})
                 return collectRaison.stop()
             }
+            collectRaison.stop()
+        })
 
-            // Demande du temps
+        console.log(collectRaison.checkEnd())
+
+        if(!collectRaison.checkEnd()) return
+
+         // Demande du temps
+
+       message.channel.send({embeds: [new Discord.EmbedBuilder({
+            title: "Bannissement de membre",
+            description: "Veuillez fournir un temps de ban ci dessous s'il vous plait !\nPour annuler la commande, veuillez taper `annuler`\nPour bannir defitivement, veuillez taper `0`",
+            // thumbnail: client.user.avatarURL()
+        })]})
+
+        time = null
+        timeFinal = null
+        const collectTime = message.channel.createMessageCollector({time: 120000})
+
+        collectTime.on("collect", msg => {
+            if(msg.author.id != message.author.id) return
+
+            if(msg.content == "annuler"){
+                message.channel.send({embeds: [new Discord.EmbedBuilder({
+                    title: "Bannissement de membre",
+                    description: "Commande annulée !",
+                    //thumbnail: client.user.avatarURL()
+                })]})
+                return collectTime.stop()
+            }
+
+            /*if(ms(msg.content) == undefined || msg.content.includes("-")){
+                console.log("On est la")
+                while(ms(msg.content) == undefined || msg.content.includes("-")){
+                    console.log("Petit soucis")
+                    message.channel.send(`:warning: • Désolé, le temps de ban renseigné n'est pas valide !`)
+                        
+                }
+            }
+
+            if(ms(msg.content) == undefined || msg.content.includes("-")) 
+
+            time = msg.content
+
+            if(time = 0) timeFinal = "définitif"
+            else timeFinal = `de ${time}`*/
+            collectTime.stop()
+        })
+
+            // Application du ban
+
+            list = message.mentions.members.map(e => " - " + e.user.tag).toString().replaceAll(",", "\n")
+
+            const row = new Discord.ActionRowBuilder()
+            .addComponents(
+                new Discord.ButtonBuilder()
+                .setCustomId("yes")
+                .setLabel("OUI, banni moi ça !")
+                .setStyle(Discord.ButtonStyle.Danger),
+
+                new Discord.ButtonBuilder()
+                .setCustomId("no")
+                .setLabel("NON, ils sont cools en fait !")
+                .setStyle(Discord.ButtonStyle.Primary)
+            )
 
             message.channel.send({embeds: [new Discord.EmbedBuilder({
                 title: "Bannissement de membre",
-                description: "Veuillez fournir un temps de ban ci dessous s'il vous plait !\nPour annuler la commande, veuillez taper `annuler`\nPour bannir defitivement, veuillez taper `0`",
-                // thumbnail: client.user.avatarURL()
-            })]})
+                description: `Voulez vous bannir:\n${list}\nAvec comme raison \`${raison}\` et avec un temps de ban \`${timeFinal}\` ?`,
+                // thumbnail: client.user.avatarURL(),
+            })], components: [row]})
 
-            time = null
-            timeFinal = null
-            const collectTime = message.channel.createMessageCollector({time: 120000})
+            const collectButtons = message.channel.createMessageComponentCollector({time: 120000})
 
-            collectTime.on("collect", msg => {
-                console.log("Oui, on est ici")
-                if(msg.author.id != message.author.id) return
-                console.log("Et meme ici !")
+            collectButtons.on("collect", button => {
+                if(message.author.id != button.member.user.id) return
+                if(button.customId == "no"){
 
-                if(ms(msg.content) == undefined || msg.content.includes("-")) message.channel.send(`:warning: • Désolé, le temps de ban renseigné n'est pas valide !`)
-
-                time = msg.content
-
-                if(time == "annuler"){
                     message.channel.send({embeds: [new Discord.EmbedBuilder({
                         title: "Bannissement de membre",
-                        description: "Commande annulée !",
-                        // thumbnail: client.user.avatarURL()
+                        description: `Commande annulée !`,
+                        // thumbnail: client.user.avatarURL(),
                     })]})
-                    return collectTime.stop()
                 }
 
-                if(time = 0) timeFinal = "définitif"
-                else timeFinal = `de ${time}`
+                if(button.customId == "yes"){
 
-                // Application du ban
+                    message.mentions.members.forEach(async element => {
+                        if(time != 0) await dbTempBan.set(element.id, message.createdTimestamp + ms(time))
+                        element.ban({reason: raison})
+                    });
 
-                list = message.mentions.members.map(e => " - " + e.user.tag).toString().replaceAll(",", "\n")
-
-                const row = new Discord.ActionRowBuilder()
-                .addComponents(
-                    new Discord.ButtonBuilder()
-                    .setCustomId("yes")
-                    .setLabel("OUI, banni moi ça !")
-                    .setStyle(Discord.ButtonStyle.Danger),
-
-                    new Discord.ButtonBuilder()
-                    .setCustomId("no")
-                    .setLabel("NON, ils sont cools en fait !")
-                    .setStyle(Discord.ButtonStyle.Primary)
-                )
-
-                message.channel.send({embeds: [new Discord.EmbedBuilder({
-                    title: "Bannissement de membre",
-                    description: `Voulez vous bannir:\n${list}\nAvec comme raison \`${raison}\` et avec un temps de ban \`${timeFinal}\` ?`,
-                    // thumbnail: client.user.avatarURL(),
-                })], components: [row]})
-
-                const collectButtons = message.channel.createMessageComponentCollector({time: 120000})
-
-                collectButtons.on("collect", button => {
-                    if(message.author.id != button.member.user.id) return
-                    if(button.customId == "no"){
-
-                        message.channel.send({embeds: [new Discord.EmbedBuilder({
-                            title: "Bannissement de membre",
-                            description: `Commande annulée !`,
-                            // thumbnail: client.user.avatarURL(),
-                        })]})
-
-                    }
-                    if(button.customId == "yes"){
-
-                        message.mentions.members.forEach(async element => {
-                            if(time != 0) await dbTempBan.set(element.id, message.createdTimestamp + ms(time))
-                            element.ban({reason: raison})
-                        });
-
-                        message.channel.send({embeds: [new Discord.EmbedBuilder({
-                            title: "Bannissement de membre",
-                            description: `Dites au revoir !`,
-                            // thumbnail: client.user.avatarURL(),
-                        })]})
+                    message.channel.send({embeds: [new Discord.EmbedBuilder({
+                        title: "Bannissement de membre",
+                        description: `Dites au revoir !`,
+                        // thumbnail: client.user.avatarURL(),
+                    })]})
 
 
-                    }
-                })
-
+                }
             })
-
-        })
 
     }
 
